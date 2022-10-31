0e5a375167e996777d10447b07741d2b293d9dff
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-10-31 14:37:10 +0200

    Initializing first setup.

---
+++ src/main/java/app/core/untitledmerchwebsite/UntitledMerchWebsiteApplication.java
@@ -0,0 +1,13 @@
+package app.core.untitledmerchwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledMerchWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledMerchWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/core/untitledmerchwebsite/UntitledMerchWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package app.core.untitledmerchwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledMerchWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
