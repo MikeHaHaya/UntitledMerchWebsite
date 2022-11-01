5250a63c2f9e2a81d5becb5ac3c04f77349a0acb
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-11-01 18:22:07 +0200

    BusinessClient entity Spring setup.

--- src/main/java/app/core/entities/clients/BusinessClient.java
+++ src/main/java/app/core/entities/clients/BusinessClient.java
@@ -1,18 +1,58 @@
 package app.core.entities.clients;
 
 import app.core.entities.merch.Merch;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class BusinessClient extends Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name, email, password;
-
     @OneToMany(cascade = CascadeType.PERSIST)
     private List<Merch> merchList;
+
+    @Override
+    public String toString() {
+        return "BusinessClient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", merchList=" + merchList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BusinessClient that)) return false;
+
+        if (getId() != that.getId()) return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        if (getEmail() != null ? !getEmail().equals(that.getEmail()) : that.getEmail() != null) return false;
+        if (getPassword() != null ? !getPassword().equals(that.getPassword()) : that.getPassword() != null)
+            return false;
+        return getMerchList() != null ? getMerchList().equals(that.getMerchList()) : that.getMerchList() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getEmail() != null ? getEmail().hashCode() : 0);
+        result = 31 * result + (getPassword() != null ? getPassword().hashCode() : 0);
+        result = 31 * result + (getMerchList() != null ? getMerchList().hashCode() : 0);
+        return result;
+    }
 }
