9eb387508207de15d3fac26fe05f74c9780846da
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-11-02 11:17:36 +0200

    PrivateClient entity

--- src/main/java/app/core/entities/clients/PrivateClient.java
+++ src/main/java/app/core/entities/clients/PrivateClient.java
@@ -1,4 +1,43 @@
 package app.core.entities.clients;
 
+import app.core.entities.merch.Merch;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class PrivateClient extends Client {
+
+    @ManyToMany(fetch = FetchType.LAZY) // TODO -- Find the best way to do this
+    @JoinTable(name = "p-clients_merch", joinColumns = @JoinColumn(name = "p-clients_id"), inverseJoinColumns = @JoinColumn(name = "merch_id"))
+    private List<Merch> merchList;
+
+    @Override
+    public String toString() {
+        return "PrivateClient{" +
+                "id=" + this.getId() +
+                ", name='" + this.getName() + '\'' +
+                ", email='" + this.getEmail() + '\'' +
+                ", password='" + this.getPassword() +  '\'' +
+                ", merchList=" + this.getMerchList() +
+                '}';
+    }
+
+    // TODO -- Set equals and hashCode correctly
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
 }
