47c3553cf828c5e86013be4c0bd790abb25452f2
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-11-02 15:55:50 +0200

    ClientRepo methods

--- src/main/java/app/core/repositories/clients/ClientRepo.java
+++ src/main/java/app/core/repositories/clients/ClientRepo.java
@@ -1,9 +1,18 @@
 package app.core.repositories.clients;
 
+import app.core.entities.clients.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("clientRepo")
-public abstract interface ClientRepo {
+public abstract interface ClientRepo extends JpaRepository<Client, Integer> {
 
+    boolean existsClientByEmailAndPassword(String email, String password);
+
+    boolean existsClientByEmail(String email);
+
+    boolean existsClientByName(String name);
+
+    Client findClientByEmailAndPassword(String email, String password);
 
 }
